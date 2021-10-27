4274c48581790088b26db07ad2ac079aa564f73c
Abraham Jerry Kakooza <abrahamkakooza@gmail.com>
2021-10-27 16:15:01 +0300

    New changes

---
+++ wsgi.py
@@ -0,0 +1,5 @@
+import date, ranges, lugandatime, currency, phonenumber, measurement, abbreviations, ordinals, cardinals, cardinals_orig, detection
+from main import app
+
+if __name__ == '__main__':
+    app.run()
